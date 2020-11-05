8d1902398f81c424cde8df6a719fdba44bd6c47f
Vishal Mali <vishalmali06@gmail.com>
2020-11-05 15:25:45 +0530

    default starter... + 6

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,10 +39,7 @@ const IndexPage = () => {
     <Layout>
       <NetlifyIdentity />
       <SEO title="Home" />
-      <h1>Hi people</h1>
       <h2 onClick={() => { openNetlifyModal() }}>Login</h2>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
